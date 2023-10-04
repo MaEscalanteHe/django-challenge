d209250b67adcc3edd34c871011bf9734088d1df
Manuel Escalante <maescalantehe@gmail.com>
2023-10-04 18:35:20 -0400

    style: install linter and apply

--- products/migrations/0001_initial.py
+++ products/migrations/0001_initial.py
@@ -4,22 +4,38 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Product',
+            name="Product",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=255)),
-                ('description', models.TextField()),
-                ('price', models.DecimalField(decimal_places=2, max_digits=10)),
-                ('quantity', models.IntegerField()),
-                ('category', models.CharField(choices=[('Electronics', 'Electronics'), ('Clothing', 'Clothing'), ('Food', 'Food')], max_length=255)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=255)),
+                ("description", models.TextField()),
+                ("price", models.DecimalField(decimal_places=2, max_digits=10)),
+                ("quantity", models.IntegerField()),
+                (
+                    "category",
+                    models.CharField(
+                        choices=[
+                            ("Electronics", "Electronics"),
+                            ("Clothing", "Clothing"),
+                            ("Food", "Food"),
+                        ],
+                        max_length=255,
+                    ),
+                ),
             ],
         ),
     ]
